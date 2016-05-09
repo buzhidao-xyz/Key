5e4810657fd10dfb00577363b742b27f165d4508
imbzd <luochuan.wang@gmail.com>
2016-05-09 10:45:07 +0800

    Account过滤规则调整
    Account过滤规则调整

--- AnyPHP/Library/Org/Util/Filter.class.php
+++ AnyPHP/Library/Org/Util/Filter.class.php
@@ -11,10 +11,10 @@ class Filter
     //初始化
     public function __construct(){}
 
-    //过滤规则：字母开始、字母数字_-.组合、长度5-20
+    //过滤规则：字母开始、字母数字_-.组合、长度3-20
     static public function F_CharNoShort($var=null)
     {
-        $regexp = "/^[a-zA-Z][a-zA-Z0-9_-.]{4,19}$/i";
+        $regexp = "/^[a-zA-Z][a-zA-Z0-9_-.]{2,19}$/i";
         if (preg_match($regexp, $var) == 0) {
             return false;
         }
